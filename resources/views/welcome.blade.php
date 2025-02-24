8bb2bb45dfbe17d06914e5e88a2a22f347134ec9
omthapa779 <omthapa781@gmail.com>
2025-02-24 18:02:43 +0545

    Hero Section Completed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.navs')
 @section('title', 'Home')
 @section('content')
-<div class="hero_section w_100 h_70vh flex_c flex_cl padding_s10 gap_xs">
+<section name="Hero Section" class="hero_section w_100 h_70vh flex_c flex_cl padding_s10 gap_xs">
     <h1 class="text_ac hero_text hero_main_text">Bridging Global Workforce Needs With <span class="secondary_text">Nepal’s Finest Talent!</span></h1>
     <h3 class="font_w500 text_ac secondary_font  gray_cl hero_text hero_subtitle">From skilled professionals to dedicated workers, we connect you with the right manpower for success. Let’s build your dream team today!</h3>
     <div class="button_holder w_100 h_fc flex_c gap_xs">
         <a href="#" class="primary_button"><h4 class="alt_text">Company's Profile</h4></a>
-        <a href="#" class="primary_button secondary_button"><h4 class="">Explore Demands</h4></a>
+        <a href="#" class="primary_button secondary_button"><h4 class="">Get Started</h4></a>
     </div>
-</div>
+</section>
+<section class="destinations w_100 h_fc"></section>
 @endsection
